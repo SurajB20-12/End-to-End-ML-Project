8af06b40c4e5a27947aed6be107b127d3240e143
Suraj20 <bhosalesuraj2012@gmail.com>
2025-03-02 11:08:25 +0530

    commit changes

--- application.py
+++ application.py
@@ -8,13 +8,12 @@ from sklearn.preprocessing import StandardScaler
 
 application=Flask(__name__)
 
-app=application
 
-@app.route('/')
+@application.route('/')
 def index():
   return render_template('index.html')
 
-@app.route('/predictdata',methods=['GET','POST'])
+@application.route('/predictdata',methods=['GET','POST'])
 def predict_datapoint():
   if request.method =='GET':
     return render_template('home.html')
@@ -39,4 +38,4 @@ def predict_datapoint():
     return render_template('home.html',result=results[0])
   
 if __name__=='__main__':
-  app.run(host="0.0.0.0",port=5000,debug=True)
+  application.run(host="0.0.0.0",port=5000,debug=True)
